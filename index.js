0a2befca0821deaec3840eb0df7095919dfea24a
Will <willchesson@gmail.com>
2022-01-07 10:08:23 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+const recipients = (["Guadalupe", "Ollie", "Aki"], surprise);
+
+
+function writeCards(recipients) {
+    for (let i = 0; i < recipients.length; i++) {
+        console.log(`Thank you, ${recipients[i]}, for the wonderful surprise gift!`);
+        debugger;
+    }
+
+    return recipients;
+}
+writeCards(recipients);
+
+function countDown( i ) {
+    while (i > 0) {
+        console.log(i);
+        i -= 1;
+    }
+    console.log(i);
+}
